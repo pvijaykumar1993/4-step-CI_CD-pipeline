e6fbc5addac929519109a3fc5d61037ab036964a
pvijaykumar1993 <vijaykumar.pepakayala@gmail.com>
2024-12-04 13:21:57 +0530

    Update main.tf

--- terrform/prod/main.tf
+++ terrform/prod/main.tf
@@ -1,1 +1,28 @@
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source = "../modules/vpc"
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_instance" "prod_instance" {
+  ami             = var.ami_id
+  instance_type   = var.instance_type
+  availability_zone = "us-east-1d"
+  subnet_id       = module.vpc.subnet_id
+
+  tags = {
+    Name = "Prod Instance"
+  }
+}
+
+output "prod_instance_id" {
+  value = aws_instance.prod_instance.id
+}
+
+output "prod_instance_public_ip" {
+  value = aws_instance.prod_instance.public_ip
+}
 
