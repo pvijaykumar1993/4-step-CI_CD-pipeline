582a3d37243847889839d6c416c521f683d35970
pvijaykumar1993 <vijaykumar.pepakayala@gmail.com>
2024-12-04 13:11:12 +0530

    Update main.tf

--- terrform/qa/main.tf
+++ terrform/qa/main.tf
@@ -1,1 +1,24 @@
+# /infrastructure/qa/main.tf
+provider "aws" {
+  region = var.region
+}
 
+module "vpc" {
+  source = "../modules/vpc"
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_instance" "qa_instance" {
+  ami             = var.ami_id
+  instance_type   = var.instance_type
+  availability_zone = "us-east-1b"
+  subnet_id       = module.vpc.subnet_id
+
+  tags = {
+    Name = "QA Instance"
+  }
+}
+
+output "qa_instance_id" {
+  value = aws_instance.qa_instance.id
+}
