c3eb46275ba44c719515fd68e1d4c288dafe9e22
pvijaykumar1993 <vijaykumar.pepakayala@gmail.com>
2024-12-04 13:07:59 +0530

    Update main.tf

--- terrform/dev/main.tf
+++ terrform/dev/main.tf
@@ -1,1 +1,23 @@
+provider "aws" {
+  region = var.region
+}
 
+module "vpc" {
+  source = "../modules/vpc"
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_instance" "dev_instance" {
+  ami             = var.ami_id
+  instance_type   = var.instance_type
+  availability_zone = "us-east-1a"
+  subnet_id       = module.vpc.subnet_id
+
+  tags = {
+    Name = "Dev Instance"
+  }
+}
+
+output "dev_instance_id" {
+  value = aws_instance.dev_instance.id
+}
